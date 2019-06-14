8ad038e30f19ab5bbb35395335628a0ddb56977f
fengwei <fengwei029@foxmail.com>
2019-06-14 10:47:47 +0800

    update App.js construct

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router, Link, Route, Redirect } from 'react-router-dom';
+import { HashRouter as Router, Link, Route, Redirect } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import TodoList from './component/TodoList';
 import About from './component/About';
 
-const App = (
-  <Provider store={store}>
-    <Router>
-      <ul>
-        <li><Link to="/todoList">TodoList</Link></li>
-        <li><Link to="/about">About</Link></li>
-      </ul>
+function App() {
+  return (
+    <Provider store={store}>
+      <Router>
+        <ul>
+          <li><Link to="/todoList">TodoList</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
 
-      <Redirect from="/" to="/todoList" />
-      <Route exact path="/todoList" component={TodoList} />
-      <Route exact path="/about" component={About} />
-    </Router>
-  </Provider>
-);
+        <Redirect from="/" to="/todoList"/>
+        <Route exact path="/todoList" component={TodoList}/>
+        <Route exact path="/about" component={About}/>
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import reactDOM from 'react-dom';
 import App from './App';
 
 reactDOM.render(
-  App,
+  <App />,
   document.getElementById('root')
 )
